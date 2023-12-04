1e85163d3d397269674fca308ef6d728deeef27d
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2023-12-05 01:34:41 +0530

    Feature: moving tsdoc html files into module specific folders

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,49 @@ import * as fs from "fs";
 import * as path from "path";
 import findDirectories, { dirDetailsArray } from "./utils/findDirectories";
 
-interface dirDetails {
-  dirPath: string;
-  isDir: boolean;
-}
-
-const readDocsFolder = async (tsdocsDir: string | null): Promise<void> => {
-  if (typeof tsdocsDir !== "string") {
-    return console.log("use a valid path"); // Assume it's not a directory if check fails
-  }
-  const data: dirDetailsArray | void = await new Promise((resolve) =>
+const getDirDetails = async (dir: string) => {
+  return await new Promise((resolve) =>
     fs.readdir(
-      tsdocsDir,
+      dir,
       async (err: NodeJS.ErrnoException | null, files: string[]) => {
         if (err) {
           return console.log(err);
         }
         const folders = files.map((file) => {
-          return path.join(tsdocsDir, file);
+          return path.join(dir, file);
         });
         resolve(await findDirectories(folders));
       },
     ),
   );
-  const filesArray = data?.map((obj: dirDetails) => obj.dirPath);
-  console.log("the data is: ", filesArray);
-  const outputFolder = "./outputHtmlDir";
-  createFolderIfNotExists(outputFolder);
-  moveHtmlFiles(filesArray, tsdocsDir, outputFolder);
 };
 
-readDocsFolder("./tsdocs/functions");
+const readRootDocsFolder = async (): Promise<void> => {
+  const tsdocsDir = process.argv[2]; // Fetching the folder path from command-line arguments
+  if (typeof tsdocsDir !== "string") {
+    return console.log("Please provide a valid folder path."); // Assume it's not a directory if check fails
+  }
+  const data = await getDirDetails(tsdocsDir);
+  if (Array.isArray(data)) {
+    const filesArray = data?.filter(
+      (obj) => obj.isDir && !obj.dirPath.includes("assets"),
+    );
+
+    filesArray.forEach(async (htmlFolder) => {
+      const data = (await getDirDetails(
+        "./" + htmlFolder.dirPath,
+      )) as dirDetailsArray;
+      const filesArray = data.map((obj) => obj.dirPath);
+      filesArray?.forEach((filePath: string) => {
+        const outputFolder = "./outputHtmlDir";
+        createFolderIfNotExists(outputFolder);
+        moveHtmlFiles(filesArray, filePath, outputFolder);
+      });
+    });
+  }
+};
+
+readRootDocsFolder();
 
 function createFolderIfNotExists(folderPath: string) {
   if (!fs.existsSync(folderPath)) {
@@ -46,19 +58,21 @@ function moveHtmlFiles(
   destination: string,
 ) {
   if (!Array.isArray(files)) {
-    return console.log("no files array found");
+    return console.log("No files array found");
   }
   files.forEach((file) => {
-    const filePath = path.join(source, file);
+    const fileNameArray = file.split("/");
+    const fileName = fileNameArray[fileNameArray.length - 1];
+    const filePath = path.resolve(source);
     if (fs.existsSync(filePath) && file.endsWith(".html")) {
-      const folderName = file.split(".")[0];
+      const folderName = fileName.split(".")[0];
       const targetFolder = path.join(destination, folderName);
 
       createFolderIfNotExists(targetFolder);
 
-      const newFilePath = path.join(targetFolder, file);
+      const newFilePath = path.join(targetFolder, fileName);
 
-      fs.renameSync(filePath, newFilePath);
+      fs.copyFileSync(filePath, newFilePath);
       console.log(`Moved ${file} to ${targetFolder}`);
     }
   });
